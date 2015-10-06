983c3145f33912c006d3a0ced03bc20b7d5da618
YannaAlghamdi <yannaalghamdi@gmail.com>
2015-10-07 00:47:42 +0800

    Fixed map (added print in infinite loop.

--- src/Players/mainGame.java
+++ src/Players/mainGame.java
@@ -1,12 +1,17 @@
 package Players;
 
+import javax.swing.UIManager;
 
 public class MainGame {
 	
 	public static void main(String[] args){
+		try {
+            // Set cross-platform Java L&F (also called "Metal")
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        }
+        catch (Exception e) {}		
 		MainFrame game = new MainFrame();
-		
-		while(!game.startGame) {} // waiting for other players
+		while(!game.startGame) {System.out.print(":)");} // waiting for other players
 		game.Home = game.createCamp(game.campNo);
 		game.dummyData();
 		game.goToHomeCamp();
